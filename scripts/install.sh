94b40301b7d8d85c4d9acb6b9356f75eb21673c2
Thomas Evans <tevans3@icloud.com>
2022-08-04 01:51:32 -0500

    CI

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,57 +5,56 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
-printf -- '======================================'
-printf -- '==           Pi-Spotter             =='
-printf -- '==           Installer              =='
-printf -- '======================================'
+printf -- '======================================\n'
+printf -- '==           Pi-Spotter             ==\n'
+printf -- '==           Installer              ==\n'
+printf -- '======================================\n'
 printf -- '\n'
 printf -- 'Installing...'
 
-stty -echo;
+{ 
+  # Enable the read-write file system
+  mount -o remount,rw / 
+  mount -o remount,rw /boot
 
-# Enable the read-write file system
-mount -o remount,rw / 
-mount -o remount,rw /boot
 
+  # Create tmp file
+  rm -rf /tmp/pispotter
+  mkdir /tmp/pispotter
 
-# Create tmp file
-rm -rf /tmp/pispotter
-mkdir /tmp/pispotter
+  # Download the compiled version of pi-spotter from releases
+  wget -O /tmp/pispotter/pispotter.zip https://github.com/teevans/pi-spotter/releases/latest/download/pispotter.zip
 
-# Download the compiled version of pi-spotter from releases
-wget -O /tmp/pispotter/pispotter.zip https://github.com/teevans/pi-spotter/releases/latest/download/pispotter.zip
+  # Extract the contents
+  unzip /tmp/pispotter/pispotter.zip -d /tmp/pispotter/
 
-# Extract the contents
-unzip /tmp/pispotter/pispotter.zip -d /tmp/pispotter/
+  # Create the folder for nginx
+  rm -rf /var/www/pispotter
+  mkdir /var/www/pispotter
 
-# Create the folder for nginx
-rm -rf /var/www/pispotter
-mkdir /var/www/pispotter
+  # Copy the contents to an nginx available folder
+  cp -r /tmp/pispotter/build/* /var/www/pispotter/
 
-# Copy the contents to an nginx available folder
-cp -r /tmp/pispotter/build/* /var/www/pispotter/
+  # Copy the pi-spotter nginx file to /etc/nginx/default.d/pispotter.conf
+  rm -f /etc/nginx/default.d/pispotter.conf
+  cp -r /tmp/pispotter/build/pispotter.conf /etc/nginx/default.d/
 
-# Copy the pi-spotter nginx file to /etc/nginx/default.d/pispotter.conf
-rm -f /etc/nginx/default.d/pispotter.conf
-cp -r /tmp/pispotter/build/pispotter.conf /etc/nginx/default.d/
+  # Reload nginx
+  systemctl reload nginx
 
-# Reload nginx
-systemctl reload nginx
+  # Clean Up
+  rm -rf /tmp/pispotter
 
-# Clean Up
-rm -rf /tmp/pispotter
+  # Enable RO Filesystem
+  mount -o remount,ro / 
+  mount -o remount,ro /boot
 
-# Enable RO Filesystem
-mount -o remount,ro / 
-mount -o remount,ro /boot
-
-stty +echo;
+} > /tmp/pispotter/install.log 2>&1 
 
 ipaddress=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 printf -- '\033[32m Done! \033[0m\n'
-printf -- 'Access Pi-Spotter from the URL Below!'
+printf -- 'Access Pi-Spotter from the URL Below!\n'
 printf -- '\n'
-printf -- 'http://$ipaddress/pispotter'
+printf -- 'http://$ipaddress/pispotter\n'
 printf -- '\n'
 
